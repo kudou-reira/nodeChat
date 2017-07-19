08b2f1dc2c60d526fe6f66a0b5d65b755521d208
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-07-19 21:45:06 +0900

    added message events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+    
+//    socket.emit('createEmail', {
+//        to: 'jen@example.com',
+//        text: 'hey, this is andrew'
+//    });
+    
+    socket.emit('createMessage', {
+        from: 'byakura',
+        text: 'hello, welcome'
+    });
+});
+            
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+//socket.on('newEmail', function(email) {
+//    console.log('new email', email);
+//});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+const express = require('express');
+
 const publicPath = path.join(__dirname, '../public');
 
-var express = require('express');
 var app = express();
+//already using createServer
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port,  () => {
-   console.log('started on port ' + port) 
-})
+io.on('connection', (socket) => {
+    console.log('new user connected')
+    
+    //emit sends custom data
+//    socket.emit('newEmail', {
+//        from: 'mike@example.com',
+//        text: 'what is going on',
+//        createdAt: 123
+//    });
+    
+    socket.emit('newMessage', {
+        from: 'keita',
+        text: 'see you',
+        createdAt: 123
+    });
+    
+//    socket.on('createEmail', (newEmail) => {
+//        console.log('createEmail', newEmail);
+//    });
+    
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('user was disconnected'); 
+    });
+});
+
+server.listen(port,  () => {
+    console.log('started on port ' + port) 
+});
 
