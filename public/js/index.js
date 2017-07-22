f267f7368ae268b7eadd86c5ba835f2db8dbcc12
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-07-22 12:45:08 +0900

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        //move to bottom of messages area
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
     
@@ -28,6 +45,7 @@ socket.on('newMessage', function(message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 //    var formattedTime = moment(message.createdAt).format('k:mm');
 //    //create list item with jQuery
 //    var li = jQuery('<li></li');
@@ -53,6 +71,7 @@ socket.on('newLocationMessage', function (message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 //    var li  = jQuery('<li></li>');
 //    var a = jQuery('<a target="_blank">My current location</a>');
 //    li.text(`${message.from} (${formattedTime}): `);
